bb2bdfbbd4db4a7122d6e11bfafb8174b356aad2
narayanan1208 <narayanan6647@gmail.com>
2024-08-25 06:07:13 -0400

    2nd commit

--- emotion_detection.py
+++ emotion_detection.py
@@ -2,21 +2,7 @@ import requests
 import json
 
 def emotion_detector(text_to_analyse):
-    # Define the URL for the sentiment analysis API
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
 
-    # Create the payload with the text to be analyzed
-    myobj = { "raw_document": { "text": text_to_analyse } }
-
-    # Set the headers with the required model ID for the API
-    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-
-    # Make a POST request to the API with the payload and headers
-    response = requests.post(url, json=myobj, headers=header)
-
-    return response.text
-
-def emotion_detector_new(text_to_analyse):
     # Define the URL for the sentiment analysis API
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
 
@@ -31,18 +17,30 @@ def emotion_detector_new(text_to_analyse):
 
     # Parse the response from the API
     formatted_response = json.loads(response.text)
-    print(response.text)
 
-    # Check if 'documentSentiment' is in the response
+    # Check if 'emotionPredictions' is in the response
     if response.status_code == 200:
-        label = formatted_response['emotionPredictions']['label']
-        score = formatted_response['emotionPredictions']['score']
+        emotions = formatted_response['emotionPredictions'][0]['emotion']
+        anger_score = emotions.get('anger', 0)
+        disgust_score = emotions.get('disgust', 0)
+        fear_score = emotions.get('fear', 0)
+        joy_score = emotions.get('joy', 0)
+        sadness_score = emotions.get('sadness', 0)
 
-    # Return the label and score in a dictionary
-    return {'label': label, 'score': score}
+        # Determine the dominant emotion
+        dominant_emotion = max(emotions, key=emotions.get)
 
-# from emotion_detection  import emotion_detector
-# emotion_detector("I love this new technology")
+    return {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+    }
+
+# from emotion_detection import emotion_detector
+# emotion_detector("I am so happy I am doing this")
 # {"emotionPredictions":[{"emotion":{"anger":0.0132405795, "disgust":0.0020517302, 
 # "fear":0.009090992, "joy":0.9699522, "sadness":0.054984167}, "target":"", 
 # "emotionMentions":[{"span":{"begin":0, "end":26, "text":"I love this new technology"}, 
